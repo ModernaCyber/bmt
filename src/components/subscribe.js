f11e2e263e0e59fd5c1d8be778ece3e51ba2a927
john kash <jhnkash@gmail.com>
2021-07-15 17:24:36 +0000

    .toml and style update

--- src/components/subscribe.js
+++ src/components/subscribe.js
@@ -7,11 +7,12 @@ const Subscribe = (
       <div className="subscribe" style={{ paddingBottom: "30px"}}>
          
           <form className="form-container" action="https://api.getform.io/v1/forms/ff65b37d-7840-424b-b240-3a464ede432f?token=m9X03TcXRbmAh19p3yGTAdOTvDPRgKMyZZ2dYjsoyIWv04g2JIwVJKhEvhox" method="post"> <div><p>
-              Subscribe today and be among the first to Receive the upcoming <i>Bmw maintenance e-book.</i>
+              Subscribe today and receive maintenance tips.!
+              Also be among the first to Receive the upcoming <i>Bmw maintenance e-book.</i>
         </p></div>
             <div>
               <label htmlFor="w3lSender">Email</label>
-              <input type="email" name="w3lSender" id="w3lSender" value="email"/>
+              <input type="email" name="w3lSender" id="w3lSender"/>
             </div>
 
             <div style={{display: "flex", justifyContent: "center"}}>
