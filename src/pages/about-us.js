bae4e361399e06031657a4c16783005b5a71d53d
ModernaCyber <modernacyber@gmail.com>
2021-08-01 11:45:39 +0000

    abt'

--- src/pages/about-us.js
+++ src/pages/about-us.js
@@ -5,7 +5,7 @@ import { Link } from "gatsby"
 
 const About_us = () => (
   <Layout>
-    <div className="post-thumbnail" style={{ backgroundImage: `url('https://image.shutterstock.com/image-photo/novosibirsk-russia-october-16-2020-600w-1839033184.jpg')`, margin:"120px 15px 15px 15px" }}>
+    <div className="post-thumbnail" style={{ backgroundImage: `url('https://image.shutterstock.com/image-photo/novosibirsk-russia-october-16-2020-600w-1839033184.jpg')`, margin:"120px 15px 15px 0" }}>
       <h1>HI!</h1>
       <h1> We are the BMWEMT Team </h1>
       <div><Link to='/contact' className="button -primary">Get in touch &rarr;</Link></div>
@@ -14,27 +14,16 @@ const About_us = () => (
       <p></p>
     </div>
     </div>
-    <div className="about-content ">
-      <p>
-        <h2>Welcome to bmwemt</h2> <h3>your number one source 
-          for all things BMW [products and resourses]</h3>
-        <h3>We're dedicated to giving you the very best of product,
-           with a focus on detailed information on 
-           <strong>
-         Models, Softwares, Spares and Accessories 
-          </strong>
-         </h3> 
-         <h3>Founded in 2018 by modernacyber, bmwemt has come a long way from its
-            beginnings in East-Africa. When Modernacyber first started out, their 
-            passion for bmw automobiles drove them to seek relevant detailed information on different bmw products
-            after which bmwemt was born to offer you what we discovered.
-            we are thrilled to be able to turn our passion into our own website.
-        We hope you enjoy our products as much as we enjoy offering them to you.
+    <div className="about-content-main ">
+        <h2>Welcome to BMWEMT</h2>
+         <p><h3>Your number one source 
+          for all things BMW [products and resourses].</h3>
+        <h3>We're dedicated to giving you the very best of product,with a focus on detailed information on <strong>Models, Softwares, Spares and Accessories. </strong>
+         </h3> </p><p>
+         <h3>Founded in 2018 by modernacyber, BMWEMT has come a long way from its beginnings in East-Africa. When Modernacyber first started out, their passion for bmw automobiles drove them to seek relevant detailed information on different bmw products after which BMWEMT was born to offer you what we discovered. We are thrilled to be able to turn our passion into our own website. We hope you enjoy our products as much as we enjoy offering them to you.
         </h3>
-        
-         If you have any questions or comments, please don't hesitate to <Link to ="/contact">contact us</Link>
-      </p>
-
+        </p>
+         If you have any questions or comments, please don't hesitate to <Link to ="/contact">contact-us</Link>
     </div>
   </Layout>
 )
