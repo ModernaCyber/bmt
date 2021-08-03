1f915b1b512105f4ca4eba764d7cd01a7c6dfe2e
Moderna Cyber <modernacyber@gmail.com>
2021-08-03 16:34:08 +0300

    remv

--- src/components/rightads.js
+++ src/components/rightads.js
@@ -12,8 +12,7 @@ const Rightads = (
 ifr&linkID=7d96f977ed9a1310cce5cf8370f34ccb&t=bmwemt-20&tracking_id=bmwemt-20"
  style={{width:"300px",height:"250px",border:"none", marginwidth:"0", marginheight:"0", scrolling:"no", frameborder:"0"}}>
    </iframe></div>
-<div><iframe  style={{width:"300px",height:"250px",border:"none", marginwidth:"0", marginheight:"0", scrolling:"no", frameborder:"0"}} src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ac&ref=tf_til&ad_type=product_link&tracking_id=bmwemt-20&marketplace=amazon&amp;region=US&placement=B082VY7N1Y&asins=B082VY7N1Y&linkId=39f61f6fa659d7006f6bf25592eb62ec&show_border=true&link_opens_in_new_window=true&price_color=333333&title_color=0066c0&bg_color=ffffff">
-    </iframe></div>
+
 <div><iframe src="//rcm-na.amazon-adsystem.com/e/cm?o=1&p=12&l=ur1&category=software&banner=1QKCM8ATVRR83BZEV5R2&f=ifr&linkID=7c33b4e5af14ad109d89a89974258733&t=bmwemt-20&tracking_id=bmwemt-20" 
 style={{width:"300px",height:"250px",border:"none", marginwidth:"0", marginheight:"0", scrolling:"no", frameborder:"0"}}></iframe></div>
       
